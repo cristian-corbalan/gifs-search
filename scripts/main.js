bf006ea96bb35d2a04c1b346c8cf207f3a1c97c4
cristian-corbalan <ccorbalandw@gmail.com>
2023-07-20 23:22:23 -0300

    Get searched GIFs and create images

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 'use strict'
-const giphyUrl = 'https://api.giphy.com/v1/gifs/random?api_key=nERS8Q07MBJm7SOwgL9xDdcPyMr95agG&tag=&rating=g'
+// Random GiF
+// const giphyUrl = 'https://api.giphy.com/v1/gifs/random?api_key=nERS8Q07MBJm7SOwgL9xDdcPyMr95agG&tag=&rating=g'
+
+const GiphyUrl = 'https://api.giphy.com/v1/gifs/search?api_key=nERS8Q07MBJm7SOwgL9xDdcPyMr95agG&q=@@value@@&limit=8&offset=0&rating=g&lang=es&bundle=messaging_non_clips';
 const inputSearch = document.getElementById('searchInput');
 const formSearch = document.getElementById('searchForm');
 const divContent = document.getElementById('content');
@@ -8,11 +11,28 @@ formSearch.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log("Searching...");
 
+    // Get value
     let value = inputSearch.value;
+    value = value.replace(/ /g, '+');
 
-    fetch(giphyUrl)
+    // Create URL with the value
+    let url = GiphyUrl.replace('@@value@@', value)
+
+    console.info(url);
+
+    fetch(url)
         .then(res => res.json())
         .then(res => {
-            console.info(res.data.images);
+            // console.info(res.data);
+
+            divContent.innerHTML = '';
+
+            for (const item of res.data) {
+                console.log(item.images)
+                let img = document.createElement('img');
+                img.setAttribute('src', item.images.fixed_width.url);
+                img.setAttribute('alt', item.title);
+                divContent.appendChild(img);
+            }
         })
 })
