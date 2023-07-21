f532fa5397ad85f267ef74cabdbe467b5e3cc9d5
cristian-corbalan <ccorbalandw@gmail.com>
2023-07-20 23:37:59 -0300

    Create a semantic structure to show the images

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@
 // Random GiF
 // const giphyUrl = 'https://api.giphy.com/v1/gifs/random?api_key=nERS8Q07MBJm7SOwgL9xDdcPyMr95agG&tag=&rating=g'
 
-const GiphyUrl = 'https://api.giphy.com/v1/gifs/search?api_key=nERS8Q07MBJm7SOwgL9xDdcPyMr95agG&q=@@value@@&limit=8&offset=0&rating=g&lang=es&bundle=messaging_non_clips';
+const GiphyUrl = 'https://api.giphy.com/v1/gifs/search?api_key=nERS8Q07MBJm7SOwgL9xDdcPyMr95agG&q=@@value@@&limit=25&offset=0&rating=g&lang=es&bundle=messaging_non_clips';
 const inputSearch = document.getElementById('searchInput');
 const formSearch = document.getElementById('searchForm');
-const divContent = document.getElementById('content');
+const ulContent = document.getElementById('content');
 
 formSearch.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -23,16 +23,20 @@ formSearch.addEventListener('submit', (event) => {
     fetch(url)
         .then(res => res.json())
         .then(res => {
-            // console.info(res.data);
-
-            divContent.innerHTML = '';
+            // Empty contents
+            ulContent.innerHTML = '';
 
             for (const item of res.data) {
                 console.log(item.images)
+
+                let li = document.createElement('li');
+                ulContent.appendChild(li);
+
                 let img = document.createElement('img');
-                img.setAttribute('src', item.images.fixed_width.url);
+                img.setAttribute('class', 'rounded-mg');
+                img.setAttribute('src', item.images.fixed_height.url);
                 img.setAttribute('alt', item.title);
-                divContent.appendChild(img);
+                li.appendChild(img);
             }
         })
 })
